67bb7ba3bc95a545151b19b17877930895444199
Brandon Ellingson <ellingsonbrandon33@gmail.com>
2025-06-01 16:03:25 -0500

    Created variables and used them in a sentance

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string firstName = "Brandon";
+            int age = 39;
+            char middleInitial = 'L';
+            bool isEmployed = true;
+            double salary = 40000.00;
+            Decimal totalSavings = 259.74m;
+
+            Console.WriteLine($"My name is {firstName} {middleInitial}. I am {age} years old. " +
+                              $"I am currently employed: {isEmployed}. My salary is {salary:C}. " +
+                              $"I have a total savings of {totalSavings:C}.");
         }
     }
 }
